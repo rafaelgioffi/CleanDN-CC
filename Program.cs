ef3ccde0e6dcedfde56593ef221001e406e468f8
Rafael Gioffi <rafaelgioffi@gmail.com>
2024-02-06 23:58:11 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace CleanDN_CC
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string DNBan = ConfigurationSettings.AppSettings["BannedDN"];
+            string LogFile = ConfigurationSettings.AppSettings["LogFile"];
+            string Folder = ConfigurationSettings.AppSettings["FolderToProcessFiles"];
+            string FileToProcess = ConfigurationSettings.AppSettings["FileToProcess"];
+
+            string[] filesInFolder = Directory.GetFiles(Folder, FileToProcess + "*");  //procura os LHDIFs que tiverem na pasta
+            int fileQuantity = filesInFolder.Length;  //quantidade de arquivos encontrados
+            string FileWithoutBanName = "";    //nome do novo arquivo SEM o(s) DN(s) banidos
+            string FileWithBanName = "";    //nome do novo arquivo COM o(s) DN(s) banidos
+
+            List<string> FileWithoutBan = new List<string>();
+            List<string> FileWithBan = new List<string>();
+
+            if (fileQuantity > 0)
+            {
+                Log("########## Inicio do processamento ##########", true);
+                Log($"Iniciando o processamento de {fileQuantity} arquivos.",false);
+                int counter = 1;
+                foreach (var file in filesInFolder) //processa arquivo por arquivo encontrado na pasta...
+                {
+                    Log($"Processando o arquivo {counter}/{fileQuantity}...", false);
+                    try
+                    {
+                        string[] allLines = File.ReadAllLines(file);
+
+                        foreach (var l in allLines) //validação de cada linha...
+                        {
+                            string DnActual = l.Substring(0, 6);
+
+                            if (DnActual == DNBan)
+                            {
+                                FileWithBan.Add(l);
+                            }
+                            else
+                            {
+                                FileWithoutBan.Add(l);
+                            }
+                        }
+
+                        if (FileWithBan.Count > 0)  //só executa alguma ação se encontrar algum DN banido...
+                        {
+                            string[] actual = file.Split('\\');
+                            string actualName = actual.Last();
+                            actual = actualName.Split('.');
+                            actualName = $"{actual[0]}.{actual[1]}";
+                            //FileWithBanName = $"{FileToProcess}.D{DateTime.Now.ToString("yyyyMMdd")}.T{DateTime.Now.ToString("HHmmss")}.txt";   //nome do arquivo com ban...
+                            FileWithBanName = $"{actualName}.D{DateTime.Now.ToString("yyyyMMdd")}.T{DateTime.Now.ToString("HHmmss")}.txt";   //nome do arquivo com ban...
+
+                            using (StreamWriter sw = new StreamWriter(Folder + FileWithBanName)) //cria o arquivo somente dos banidos...
+                            {
+                                foreach (string newLines in FileWithBan)
+                                {
+                                    sw.WriteLine(newLines);
+                                }
+                            }
+                            Log($"Gerado o arquivo com DNs banidos {FileWithBanName}...",false);
+                            
+                            Thread.Sleep(3000); //intervalo de 3s por segurança...
+
+                            FileWithoutBanName = $"{actualName}.D{DateTime.Now.ToString("yyyyMMdd")}.T{DateTime.Now.ToString("HHmmss")}.txt";   //nome do arquivo sem ban...
+                            
+                            using (StreamWriter sw = new StreamWriter(Folder + FileWithoutBanName)) //cria o arquivo somente sem os banidos...
+                            {
+                                foreach (string newLines in FileWithoutBan)
+                                {
+                                    sw.WriteLine(newLines);
+                                }
+                            }
+                            Log($"Gerado o arquivo sem os DNs banidos {FileWithoutBanName}...",false);
+
+                            string BkpOriginalFile = $"{actualName}.D{DateTime.Now.ToString("yyyyMMdd")}.T{DateTime.Now.ToString("HHmmss")}.txt.old";
+                            File.Move(file,Folder + BkpOriginalFile);    //renomeia o arquivo original para não ser processado...
+                            Log($"Arquivo {actualName}.txt renomeado para {BkpOriginalFile}...", false);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Erro ao processar o arquivo {file}.\n{ex.Message}");
+                        Log($"Erro ao processar o arquivo {file}. {ex.Message}", false);
+                    }
+                    counter++;
+                }
+                Log("########## Fim do processamento ##########\n", true);
+            }
+            else
+            {
+                Log($"Nenhum arquivo para processar em {Folder}", false);
+                Console.WriteLine($"Nenhum arquivo para processar em {Folder}");
+            }
+
+            void Log(string message, bool special)
+            {
+                using (StreamWriter swLog = new StreamWriter(LogFile, true))
+                {
+                    if (special)
+                    {
+                        swLog.WriteLine(message);
+                    }
+                    else
+                    {
+                    swLog.WriteLine($"{DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss")} => {message}");
+                    }
+                }
+            }
+
+        }
+    }
+}
